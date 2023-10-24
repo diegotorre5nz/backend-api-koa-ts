e1aaa9b8b3b2c2f9a16ef2c391fc4ab5337db36f
dtorres <diegotorre5nz@gmail.com>
2023-10-24 12:11:48 -0600

    fix: Fix controller validation types

--- src/middleware/controller-validations.ts
+++ src/middleware/controller-validations.ts
@@ -1,14 +1,13 @@
 import type { Context, Middleware, Next } from 'koa'
-import jsonschema, {ValidatorResult, ValidationError as JsonValidationError} from 'jsonschema'
+import jsonschema, {Validator, ValidatorResult, ValidationError as JsonValidationError} from 'jsonschema'
 import { ValidationError } from '../utils/errors'
-import { JSONSchema, JsonObjectOrFieldExpression } from 'objection'
 const logger = require('../utils/logger')
 
 export function validate( schema: any): Middleware {
   return async (ctx: Context, next: Next): Promise<Middleware> => {
-      const validator = new jsonschema.Validator()
-      const inputData = ctx.request.body
-      const ValidatorResult = validator.validate(inputData, schema)
+      const validator: Validator = new jsonschema.Validator()
+      const inputData: Object = ctx.request.body
+      const ValidatorResult: ValidatorResult = validator.validate(inputData, schema)
       const validationErrors: JsonValidationError[] = ValidatorResult.errors
       if (validationErrors.length > 0) {
         throw new ValidationError(validationErrors[0].toString())
