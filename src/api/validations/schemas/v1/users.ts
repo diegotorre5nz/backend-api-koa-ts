bd262d5e8372b53e5d80b865615e2cde1c171544
dtorres <diegotorre5nz@gmail.com>
2023-10-23 22:02:26 -0600

    feat: Add user validation schema

---
+++ src/api/validations/schemas/v1/users.ts
@@ -0,0 +1,25 @@
+export const jwtToken = {
+  type: 'Object',
+  required: true,
+  properties: {
+    jwtToken: { type: 'string', required: true },
+  },
+}
+
+export const login = {
+  type: 'Object',
+  required: true,
+  properties: {
+    email: { type: 'string', required: true, format: 'email', maxLength: 80 },
+    password: { type: 'string', required: true, minLength: 8, maxLength: 80 },
+  },
+}
+
+export const create = {
+  type: 'Object',
+  required: true,
+  properties: {
+    email: { type: 'string', required: true, format: 'email', maxLength: 80 },
+    password: { type: 'string', required: true, minLength: 8, maxLength: 80 },
+  },
+}
