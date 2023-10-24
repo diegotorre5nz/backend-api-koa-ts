605b5ee6b4cd5292df19a444aa36cfe74cb50818
dtorres <diegotorre5nz@gmail.com>
2023-10-23 22:01:24 -0600

    feat: Add v1 routes

---
+++ src/api/routes/v1.ts
@@ -0,0 +1,12 @@
+import Router from 'koa-router'
+import { handleErrors, handleNotFound } from '../../middleware/errors'
+import * as controllers from '../controllers/v1'
+import { Context } from 'koa'
+
+const router = new Router<{}, Context> ({ prefix: '/v1' })
+
+router.use(handleErrors)
+router.post('/users', controllers.user.create)
+router.use(handleNotFound)
+
+export const v1Routes = router.routes()
--- src/routes/v1.ts
+++
@@ -1,8 +0,0 @@
-import Router from 'koa-router'
-import { handleErrors, handleNotFound } from '../middleware/errors'
-
-const router = new Router()
-router.use(handleErrors)
-router.use(handleNotFound)
-
-export default router.routes()
