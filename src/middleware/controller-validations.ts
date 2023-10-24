5d9fec8df96c1f832c71ade215a1c00907d5ad45
dtorres <diegotorre5nz@gmail.com>
2023-10-24 14:18:57 -0600

    fix: Fix body validator

--- src/middleware/controller-validations.ts
+++ src/middleware/controller-validations.ts
@@ -1,12 +1,11 @@
 import type { Context, Middleware, Next } from 'koa'
-import jsonschema, {Validator, ValidatorResult, ValidationError as JsonValidationError} from 'jsonschema'
+import jsonschema, {ValidatorResult, ValidationError as JsonValidationError} from 'jsonschema'
 import { ValidationError } from '../utils/errors'
-const logger = require('../utils/logger')
 
 export function validate( schema: any): Middleware {
   return async (ctx: Context, next: Next): Promise<Middleware> => {
-      const validator: Validator = new jsonschema.Validator()
-      const inputData: Object = ctx.request.body
+      const validator = new jsonschema.Validator()
+      const inputData: Object = Object(ctx.request.body)
       const ValidatorResult: ValidatorResult = validator.validate(inputData, schema)
       const validationErrors: JsonValidationError[] = ValidatorResult.errors
       if (validationErrors.length > 0) {
